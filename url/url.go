8fa02cacdeb6ae41509db98b9ab24550b9c032f9
Raihan <malfaruq777@gmail.com>
2026-05-25 20:15:51 +0700

    feat: tambah CRUD PKL dan Laporan Modul 17

--- url/url.go
+++ url/url.go
@@ -11,9 +11,11 @@ import (
 // Untuk menambah modul baru, buat file [modul]Route.go lalu panggil fungsinya di sini.
 func Web(app *fiber.App) {
 	// Global
-	app.Get("/", controller.Homepage)
+	// Pindahkan homepage API ke `/api` supaya root `/` dapat melayani frontend statis
+	app.Get("/api", controller.Homepage)
 	app.Get("/ip", controller.IPServer)
 
 	// Tambahkan route modul di bawah ini setelah PR di-merge
+	PKLRoute(app)
 	// Contoh: MahasiswaRoute(app)
 }
